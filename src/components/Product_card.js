1ab9d8e0870d19a8c57987449912ff4971b95270
Polina <106979919+kollarbone@users.noreply.github.com>
2023-04-27 11:12:25 +0300

    Update Product_card.js

--- src/components/Product_card.js
+++ src/components/Product_card.js
@@ -7,32 +7,34 @@ export default function Product_card(props) {
   const { addToCart } = useContext(CartContext);
   const data = props.data;
   return (
-    <NavLink to={`/productList/${data.id}`} data={data}>
-      <div className="flex flex-col items-center rounded border border-zinc-300 hover:border-zinc-400">
-        {data ? (
-          <>
-            <img
-              src={`${data.image}`}
-              alt={data.title}
-              className="object-cover h-48"
-            />
-            <p className="pl-1 text-ellipsis overflow-hidden h-6">
-              {data.title}
-            </p>
-            <div className="flex flex-row items-center w-full justify-around mt-3">
-              <p className="w-fit pl-2 whitespace-nowrap">{data.price} $</p>
-              <button
-                className="bg-zinc-300 rounded w-1/2 p-2 mt-auto ml-auto flex justify-center  hover:bg-zinc-400 hover:bg-inherit"
-                onClick={() => addToCart(data)}
-              >
-                <BsCart2 />
-              </button>
+    <>
+      {data ? (
+        <>
+          <NavLink to={`/productList/${data.id}`} data={data}>
+            <div className="flex flex-col items-center rounded border border-zinc-300 hover:border-zinc-400">
+              <img
+                src={`${data.image}`}
+                alt={data.title}
+                className="object-cover h-48"
+              />
+              <p className="pl-1 text-ellipsis overflow-hidden h-6">
+                {data.title}
+              </p>
+              <div className="flex flex-row items-center w-full justify-around mt-3">
+                <p className="w-fit pl-2 whitespace-nowrap">{data.price} $</p>
+                <button
+                  className="bg-zinc-300 rounded w-1/2 p-2 mt-auto ml-auto flex justify-center  hover:bg-zinc-400 hover:bg-inherit"
+                  onClick={() => addToCart(data)}
+                >
+                  <BsCart2 />
+                </button>
+              </div>
             </div>
-          </>
-        ) : (
-          <div>no data</div>
-        )}
-      </div>
-    </NavLink>
+          </NavLink>
+        </>
+      ) : (
+        <div>no data</div>
+      )}
+    </>
   );
 }
